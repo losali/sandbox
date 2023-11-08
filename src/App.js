0d8e512700c87878aaed5ba7fc08ec5e48281a6a
logan <logan.saliga@gmail.com>
2023-11-08 13:10:33 -0500

    initial commit, testing

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Logan is finally working on a site.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn to read
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
